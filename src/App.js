34a5e39399bd818fc2646e01cd843fdc681e02bf
Jesus Sanchez <jsancher@ieee.org>
2020-11-03 12:53:45 -0500

    small device banner done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './css/styles.css';
 import NavBar from './components/navigation/nav-bar'
+import Banner from './components/banner/banner'
 
 function App() {
     return (
         <div>
             <section id="inicio">
                 <NavBar/>
-                <header className="header__container"></header>
+                <header className="header__container">
+                    <Banner />
+                </header>
             </section>
             <section id="nosotros"><h1>Nosotros</h1></section>
             <section id="asesorias"><h1>Asesorías</h1></section>
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Banner() {
+    return (
+        <div className="banner__container">
+            <div className="banner__info">
+                <p className="banner__tag montserrat__font">#MejoresFinanzas</p>
+                <p className="banner__title montserrat__font">Tus cuentas en verde</p>
+                <a className="banner__btn btn montserrat__font" href="">Bioma financiero</a>
+            </div>
+            <div className="banner__img debug">
+                Img
+            </div>
+
+        </div>
+    )
+}
